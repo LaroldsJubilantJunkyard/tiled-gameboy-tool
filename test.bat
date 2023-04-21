d4321a8f336dd47a0ef0d6f96ef2b63f7d74a1ec
LaroldsJubilantJunkyard <laroldsjubilantjunkyard@gmail.com>
2023-04-21 00:27:24 -0400

    moving the tiled assets into a sub folder

--- test.bat
+++ test.bat
@@ -1,9 +1,9 @@
 "dist/windows/tiled-gameboy-tool.exe" --enable-objects --solid-map --object-field gbdk.type uint8 ^
                                       --object-field gbdkString.dialogue string ^
                                       --output-dir dist ^
-                                       World1Area1.tmx
+                                       assets/World1Area1.tmx
 
 "dist/windows/tiled-gameboy-tool.exe" --enable-objects --solid-map --export-type --rgbds     --object-field gbdk.type uint8 ^
                                                                             --object-field gbdkString.dialogue string ^
                                                                             --output-dir dist/dist2 ^
-                                                                            World1Area1.tmx
+                                                                            assets/World1Area1.tmx
