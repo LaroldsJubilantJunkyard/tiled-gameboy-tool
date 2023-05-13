7b9e916b125477a699c43bfb1ac6ca2d5093b067
LaroldsJubilantJunkyard <laroldsjubilantjunkyard@gmail.com>
2023-05-13 06:41:04 -0400

    removing old assets folder

--- assets/Enemies.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Enemies" tilewidth="8" tileheight="8" tilecount="128" columns="1">
- <image source="Enemies.png" width="8" height="16"/>
-</tileset>
