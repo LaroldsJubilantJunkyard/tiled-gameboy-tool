7de6b8a207eebf0376c136b8012a3c5437bd7b71
LaroldsJubilantJunkyard <laroldsjubilantjunkyard@gmail.com>
2023-05-12 22:00:52 -0400

    updating file utils test with linux and windows separate tests adding jest-os-detection

--- jest.config.ts
+++ jest.config.ts
@@ -5,5 +5,6 @@ const config: Config.InitialOptions = {
   transform: {
   "^.+\\.tsx?$": "ts-jest",
   },
+  setupFilesAfterEnv: ["jest-os-detection"]
 };
 export default config;
--- src/utils/file.utils.test.ts
+++ src/utils/file.utils.test.ts
@@ -21,15 +21,36 @@ describe('file utils',()=>{
     })
 
 
-    test('absolute URLs can get retrieved from relative URLs',()=>{
+    test.onLinux('absolute URLs can get retrieved from relative URLs',()=>{
+
+        expect(getAbsoluteUrl("/MyPath/Here/To/MyFile.extension")).toEqual(sep+"MyPath"+sep+"Here"+sep+"To"+sep+"MyFile.extension")
+    })
+    test.onLinux('absolute URLs can get retrieved from relative URLs',()=>{
+
+        expect(getAbsoluteUrl("Here/To/MyFile.extension","/MyOtherPath")).toEqual(sep+"MyOtherPath"+sep+"Here"+sep+"To"+sep+"MyFile.extension")
+    })
+    test.onLinux('absolute URLs can get retrieved from relative URLs using the current working directory',()=>{
+
+        const realProcess = global.process
+
+        // Mock the process.cwd() with a different directory
+        global.process = {...realProcess,cwd:()=>"/MyFinalPath"}
+
+        expect(getAbsoluteUrl("Here/To/MyFile.extension")).toEqual(sep+"MyFinalPath"+sep+"Here"+sep+"To"+sep+"MyFile.extension");
+
+        global.process=realProcess;
+    })
+
+
+    test.onWindows('absolute URLs can get retrieved from relative URLs',()=>{
 
         expect(getAbsoluteUrl("D://MyPath/Here/To/MyFile.extension")).toEqual("D:"+sep+"MyPath"+sep+"Here"+sep+"To"+sep+"MyFile.extension")
     })
-    test('absolute URLs can get retrieved from relative URLs',()=>{
+    test.onWindows('absolute URLs can get retrieved from relative URLs',()=>{
 
         expect(getAbsoluteUrl("Here/To/MyFile.extension","C://MyOtherPath")).toEqual("C:"+sep+"MyOtherPath"+sep+"Here"+sep+"To"+sep+"MyFile.extension")
     })
-    test('absolute URLs can get retrieved from relative URLs using the current working directory',()=>{
+    test.onWindows('absolute URLs can get retrieved from relative URLs using the current working directory',()=>{
 
         const realProcess = global.process
 
