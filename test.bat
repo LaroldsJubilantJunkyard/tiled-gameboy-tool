b11774193f3827d2e3336b8794c6ad3e1b219afa
LaroldsJubilantJunkyard <laroldsjubilantjunkyard@gmail.com>
2023-05-13 06:39:27 -0400

    removing extra file

--- test.bat
+++
@@ -1,9 +0,0 @@
-"dist/windows/tiled-gameboy-tool.exe" --enable-objects --solid-map --object-field gbdk.type uint8 ^
-                                      --object-field gbdkString.dialogue string ^
-                                      --output-dir dist ^
-                                       assets/World1Area1.tmx
-
-"dist/windows/tiled-gameboy-tool.exe" --enable-objects --solid-map --export-type --rgbds     --object-field gbdk.type uint8 ^
-                                                                            --object-field gbdkString.dialogue string ^
-                                                                            --output-dir dist/dist2 ^
-                                                                            assets/World1Area1.tmx
