e622f7bbb265e6297bac7bd3a3166d7af2cfbd78
LaroldsJubilantJunkyard <laroldsjubilantjunkyard@gmail.com>
2023-05-17 18:53:46 -0400

    adding linux tests and targets

--- src/tests/execution/gbdk/gbdk.execution.test.ts
+++ src/tests/execution/gbdk/gbdk.execution.test.ts
@@ -20,7 +20,33 @@ afterEach(() => {
 
 describe('gbdk execution tests',()=>{
 
-    test('tiled export',async ()=>{
+    test.onLinux('linux tiled export',async ()=>{
+
+        execSync("make run-linux-gbdk-tiled",{cwd:__dirname})
+
+        expect(existsSync(resolve(__dirname,"obj","Example.gb"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","Enemies.c"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","Enemies.h"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","world1area1.c"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","world1area1.h"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","World1Tileset.c"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","World1Tileset.h"))).toBeTruthy();
+    })
+
+    test.onLinux('linux ldtk export',async ()=>{
+
+        execSync("make run-linux-gbdk-ldtk",{cwd:__dirname})
+
+        expect(existsSync(resolve(__dirname,"obj","Example.gb"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","Enemies.c"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","Enemies.h"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","ldtk_test_project_autolayer.c"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","ldtk_test_project_autolayer.h"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","ldtk_test_project_world_level_1.c"))).toBeTruthy();
+        expect(existsSync(resolve(__dirname,"gen","ldtk_test_project_world_level_1.h"))).toBeTruthy();
+    })
+
+    test.onWindows('tiled export',async ()=>{
 
         execSync("make run-gbdk-tiled",{cwd:__dirname})
 
@@ -33,7 +59,7 @@ describe('gbdk execution tests',()=>{
         expect(existsSync(resolve(__dirname,"gen","World1Tileset.h"))).toBeTruthy();
     })
 
-    test('ldtk export',async ()=>{
+    test.onWindows('ldtk export',async ()=>{
 
         execSync("make run-gbdk-ldtk",{cwd:__dirname})
 
